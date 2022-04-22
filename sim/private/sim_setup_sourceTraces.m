ianheimbuch/tusx

commit 0fefb30bc8c096533dd2b1afd7d19e7d52b12598

Documentation and kWaveCheck
Improved documentation—mostly in the main README. Addition of a function that checks that k-Wave is on the MATLAB path before (kWaveCheck); kWaveCheck added check to tusx_sim_setup.

sim/private/sim_setup_sourceTraces.m
@@ -9,9 +9,10 @@ arguments
 end
 % % Set pressure pattern (transducer output)
 %   Define time-varying sinusoidal source/s
-source_freq = transducerSpecs.freq_Hz;      % [Hz]
+source_freq_MHz = transducerSpecs.freq_MHz; % [MHz]
+source_freq_Hz = source_freq_MHz / 1000;    % [Hz] Convert MHz to Hz
 source_mag = transducerSpecs.source_mag_Pa; % [Pa]
-source.p = source_mag * sin(2 * pi * source_freq * kgrid.t_array);
+source.p = source_mag * sin(2 * pi * source_freq_Hz * kgrid.t_array);
 
 %   Filter the source to remove high frequencies not supported by the grid
 source.p = filterTimeSeries(kgrid, medium, source.p);

gen/kWaveCheck.m
@@ -1,0 +1,21 @@
+function kWaveCheck
+% kWaveCheck Checks that k-Wave is on search path. Returns error if not
+if ~kWaveInstalled
+    m1 = 'k-Wave is not on the MATLAB search path.';
+    m2 = 'k-Wave is an open source acoustics toolbox for MATLAB (k-wave.org). TUSX is dependent on k-Wave to function.';
+    m3 = 'If you have already downloaded k-Wave, add it to the MATLAB search path.';
+    m4 = 'If you have not downloaded k-Wave, k-Wave can be downloaded at www.k-wave.org';
+    mess = strjoin({m1, m2, m3, m4}, '\n'); % Join with newlines between each
+    error(mess);
+end
+end
+
+function isOnPath = kWaveInstalled
+result = exist('k-Wave.m','file');
+switch result
+    case 2 % File 'k-Wave.m' exists on search path
+        isOnPath = true;
+    otherwise
+        isOnPath = false;
+end
+end

sim/tusx_sim_setup.m
@@ -12,7 +12,7 @@ function [kgrid, medium, source, sensor, input_args, infoStruct, grids] = ...
 %   
 %   Inputs:
 %
-%   skullNifti:     File path to nifti file
+%   skullNifti:     File path to NIfTI file
 %   scalpLocation:  3-element array of NIfTI voxel coordinates (fslX, fslY,
 %                   fslZ). Example: [104,6,122]
 %   ctxTarget:      3-element array of NIfTI voxel coordinates (fslX, fslY,
@@ -40,7 +40,11 @@ function [kgrid, medium, source, sensor, input_args, infoStruct, grids] = ...
 %   'water': Structure of acoustic values for water
 %            If set, entire acoustic medium is set to values of water (no
 %            skull)
-%   'transducer': Structure of values of transducer
+%   'transducer': Structure of values of transducer. Fields:
+%                   .focalLength_m: Focal length of the transducer
+%                   .freq_MHz: Acoustic frequency [Hertz] of the transducer
+%                   .source_mag_Pa: Magnitude [Pascals] of a single grid
+%                   point of the transducer.
 %   'record': Cell array of character arrays. To be passed to
 %             kspaceFirstOrder3D as sensor.record
 %             Default: {'p_final', 'p_max', 'p_rms'}
@@ -103,6 +107,8 @@ end
 arguments (Repeating)
     varargin
 end
+
+kWaveCheck; % Check that k-Wave is on MATLAB's search path
 
 % Parse inputs
 %   transducerSpecs, brain, and skull: structures that specify respective

commit aa9975875b3791ad6645fc1d7c4180fe127db439

Fixed pressure trace creation
Fixed a bug introduced in commit 0fefb30bc8c096533dd2b1afd7d19e7d52b12598 that caused sine wave frequency to be improperly calculated.

sim/private/sim_setup_sourceTraces.m
@@ -10,7 +10,7 @@ end
 % % Set pressure pattern (transducer output)
 %   Define time-varying sinusoidal source/s
 source_freq_MHz = transducerSpecs.freq_MHz; % [MHz]
-source_freq_Hz = source_freq_MHz / 1000;    % [Hz] Convert MHz to Hz
+source_freq_Hz = source_freq_MHz * 1e6;    % [Hz] Convert MHz to Hz
 source_mag = transducerSpecs.source_mag_Pa; % [Pa]
 source.p = source_mag * sin(2 * pi * source_freq_Hz * kgrid.t_array);
 